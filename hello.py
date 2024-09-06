40739035feeace6e7577e47f9ffffdd72d8e4093
jmnehrt <jmnehrt@gmail.com>
2024-09-06 09:51:04 -0500

    first commit

--- hello.py
+++ hello.py
@@ -4,16 +4,26 @@
 # to the console (the panel on the right)
 
 print("Hello");
-
+print("Hello World!")
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
 
 #   1. Change the message that is printed.
+print("Hello, World!")
 #   2. Figure out what the parentheses do. Will the code work without them?
+# no
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+# must close quotaiton marks. double or single don't matter, but they must close with the same as the opening. 
 #   4. Print a number. (Bonus: Print two numbers added together).
+print(7)
+print(7+2)
 #   5. Print multiple messages one after the other.
+print("Hello, World!")
+print ("have a great day!")
 #   6. Print two messages on the same line.
+print("Hello, World!", "have a great day!")
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+print('He said, "Hello, my name is Inigo Montoya..." ')
 #   8. Other. You choose!
+print("Henry" + "Lila")
 #
